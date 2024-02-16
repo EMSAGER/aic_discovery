745237b2841ab29d836798aec7764dc7b427365f
Emily <emsager7@gmail.com>
2024-02-16 14:48:46 -0800

    updated models to include favorites

--- forms.py
+++ forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, HiddenField
+from wtforms import StringField, PasswordField, EmailField, HiddenField, SelectMultipleField
 from wtforms.validators import InputRequired, Length, Email, Optional
+
+favorites = [("AfD","African Diaspora"),("Anc", "Ancient"),("An", "Animals"),
+             ("Arc", "Architecture"), ("ArA", "Arms and Armor"),("AD", "Art Deco"), 
+             ("CA", "Chicago Artists"), ("Cty", "Cityscapes"), ("DD", "Drinking and Dining"), 
+             ("Es", "Essentials"), ("Fash", "Fashion"), ("Fu", "Furniture"), ("Imp", "Impressionism"), 
+             ("LS", "Landscapes"), ("Ma", "Masks"), ("Mi", "Miniature"), ("Mo", "Modernism"),("Myth", "Mythology"), 
+             ("PA", "Pop Art"), ("Po", "Portraits"), ("SL", "Still Life"), ("Sur", "Surrealism"), ("WP", "Woodblock Print"), 
+             ("19c", "19th Century"), ("20c", "20th Century"), ("21c", "21st Century")]
 
 class UserForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(max=20)])
@@ -8,6 +16,7 @@ class UserForm(FlaskForm):
     email = EmailField("Email", validators=[InputRequired(), Length(max=50)])
     first_name = StringField("First name", validators=[InputRequired(), Length(max=30)])
     last_name = StringField("Last name", validators=[InputRequired(), Length(max=30)])
+    favorites = SelectMultipleField("Favorites", choices=favorites, validators=[Optional()])
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired(), Length(max=20)])
@@ -22,6 +31,5 @@ class UserEditForm(FlaskForm):
     
 class FavoriteForm(FlaskForm):
     """the purpose of this form is to pass CSRF TOKEN through"""
-    pass
-    HiddenField
+    csrf_token = HiddenField()
     
