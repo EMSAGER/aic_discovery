5e1854ab44a86ceee7a3a8c95d8aa9c21c2ce362
Emily <emsager7@gmail.com>
2024-02-16 11:04:26 -0800

    updated models

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False)
     first_name = db.Column(db.String(30))
     last_name = db.Column(db.String(30))
