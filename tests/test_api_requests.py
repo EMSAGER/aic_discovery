415d0561cca88cba8e46c3cf6351504cc02c8e7e
Emily <emsager7@gmail.com>
2024-04-11 12:15:48 -0700

    more filter tests

--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -75,9 +75,6 @@ class TestAPIRequests(TestCase):
         db.session.add_all([test_art_a, test_art_s, test_art_f])
         db.session.commit()
 
-
-
-
         favorite = Favorite(id=1, user_id=user.id, artist_id=1, artwork_id=test_art_s.id)
         not_favorite = NotFavorite(id=2, user_id=user.id, artist_id=2, artwork_id=test_art_a.id)
         db.session.add_all([favorite, not_favorite])
@@ -96,47 +93,47 @@ class TestAPIRequests(TestCase):
         
 
 
-    # @patch('api_requests.requests.get')
-    # def test_get_artworks(self, mock_get):
-    #     """Test get_artworks with mocked API response"""
-    #     mock_response = {
-    #         "data": [
-    #             # Mock data structure as expected from API
-    #             {
-    #                 "id": 1, 
-    #                 "title": "Test Artwork", 
-    #                 "artist_title": "Test Artist", 
-    #                 'date_start': 1902,
-    #                 'date_end': 2002,
-    #                 'medium_display': "Watercolor",
-    #                 'dimensions': "100x150",
-    #                 'image_id': "second_test_image_id",
-    #                 'image_url': "http://example.com/second_image.jpg"}
-    #         ]
-    #     }
-    #     # Set up the mock_get response
-    #     mock_get.return_value.json.return_value = mock_response
-    #     mock_get.return_value.status_code = 200
+    @patch('api_requests.requests.get')
+    def test_get_artworks(self, mock_get):
+        """Test get_artworks with mocked API response"""
+        mock_response = {
+            "data": [
+                # Mock data structure as expected from API
+                {
+                    "id": 1, 
+                    "title": "Test Artwork", 
+                    "artist_title": "Test Artist", 
+                    'date_start': 1902,
+                    'date_end': 2002,
+                    'medium_display': "Watercolor",
+                    'dimensions': "100x150",
+                    'image_id': "second_test_image_id",
+                    'image_url': "http://example.com/second_image.jpg"}
+            ]
+        }
+        # Set up the mock_get response
+        mock_get.return_value.json.return_value = mock_response
+        mock_get.return_value.status_code = 200
       
-    #     # Retrieve the user object from the database
-    #     user = User.query.get(self.user_id)
+        # Retrieve the user object from the database
+        user = User.query.get(self.user_id)
         
-    #     # Pass the user object to get_artworks
-    #     res = APIRequests.get_artworks(user)
+        # Pass the user object to get_artworks
+        res = APIRequests.get_artworks(user)
        
-    #     # Assertions about the response
-    #     self.assertEqual(len(res), len(mock_response['data']))
-    #     for artwork_data, expected in zip(res, mock_response['data']):
-    #         self.assertEqual(artwork_data.id, expected['id'])
-    #         self.assertEqual(artwork_data.title, expected['title'])
+        # Assertions about the response
+        self.assertEqual(len(res), len(mock_response['data']))
+        for artwork_data, expected in zip(res, mock_response['data']):
+            self.assertEqual(artwork_data.id, expected['id'])
+            self.assertEqual(artwork_data.title, expected['title'])
             
 
-    #     # Assert that the API was called with the correct URL and headers
-    #     mock_get.assert_called_with(
-    #         APIRequests.API_URL,
-    #         headers=APIRequests.HEADER,
-    #         params={'limit': 100, 'page': 1, 'fields': 'id,title,artist_title,image_id,dimensions,medium_display,date_display,date_start,date_end, artist_display'}
-    #     )
+        # Assert that the API was called with the correct URL and headers
+        mock_get.assert_called_with(
+            APIRequests.API_URL,
+            headers=APIRequests.HEADER,
+            params={'limit': 100, 'page': 1, 'fields': 'id,title,artist_title,image_id,dimensions,medium_display,date_display,date_start,date_end, artist_display'}
+        )
 
     @patch('api_requests.Favorite.query')
     @patch('api_requests.NotFavorite.query')
@@ -163,4 +160,6 @@ class TestAPIRequests(TestCase):
 
         # Assertions to verify the correct filtering of artworks
         self.assertEqual(len(filtered_artworks), 1)  # Only one artwork should match the criteria
-        self.assertEqual(filtered_artworks[0]['id'], self.test_art_f.id)  # Verify the ID of the artwork returned
+        self.assertEqual(filtered_artworks[0]['id'], self.test_art_f.id)
+        self.assertEqual(favorite_ids, [1]) #test the favorite.artwork.id is correct
+        self.assertEqual(not_favorite_ids, [2]) #test the not_favorite.artwork.id
