43708f7d4ee2f3e79a93040c219418e5aae5b5af
Emily <emsager7@gmail.com>
2024-05-15 13:40:03 -0700

    bug fix again

--- app.py
+++ app.py
@@ -18,11 +18,16 @@ HEADER = {
 }
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///aic_capstone"
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql://aic_capstone'))
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///aic_capstone"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config["SECRET_KEY"] = "CQASDFKLAGY4O;GHBZSDCJ245T654#%@#asthgj"
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "a3$$@09vb8aseh@#D")
+# app.config["SECRET_KEY"] = "CQASDFKLAGY4O;GHBZSDCJ245T654#%@#asthgj"
 
 toolbar = DebugToolbarExtension(app)
 
